e32622608e8733b5a22222f976c1e8303b292510
ivol <ivol@Nocturne.local>
2026-06-02 14:32:59 +0100

    Relocate BouncyCastle to io.approov.internal.volley.bouncycastle
    Use a service-layer-specific relocation prefix to prevent duplicate class
    conflicts when approov-service-volley is combined with other Approov
    service layers in the same Android application.

--- approov-service/src/main/java/io/approov/service/volley/ApproovDefaultMessageSigning.java
+++ approov-service/src/main/java/io/approov/service/volley/ApproovDefaultMessageSigning.java
@@ -24,9 +24,9 @@ import android.util.Log;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 
-import io.approov.internal.bouncycastle.asn1.ASN1InputStream;
-import io.approov.internal.bouncycastle.asn1.ASN1Integer;
-import io.approov.internal.bouncycastle.asn1.ASN1Sequence;
+import io.approov.internal.volley.bouncycastle.asn1.ASN1InputStream;
+import io.approov.internal.volley.bouncycastle.asn1.ASN1Integer;
+import io.approov.internal.volley.bouncycastle.asn1.ASN1Sequence;
 
 import java.math.BigInteger;
 import java.net.URI;
