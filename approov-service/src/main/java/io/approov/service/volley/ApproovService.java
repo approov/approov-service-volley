ba877c8043993f24b9e34f3dda9ff28b42b7b9a2
ivol <ivol@Nocturne.local>
2026-06-03 12:04:19 +0100

    refactor: simplify empty config reinitialization check using isApproovEnabled

--- approov-service/src/main/java/io/approov/service/volley/ApproovService.java
+++ approov-service/src/main/java/io/approov/service/volley/ApproovService.java
@@ -115,7 +115,7 @@ public class ApproovService {
 
         // If we are already initialized with a valid config, ignore any subsequent
         // empty config initialization
-        if (isInitialized && configString != null && !configString.isEmpty() && config.isEmpty()) {
+        if (isApproovEnabled() && config.isEmpty()) {
             Log.d(TAG, "ApproovService already initialized with a valid config; ignoring empty configuration");
             return;
         }
